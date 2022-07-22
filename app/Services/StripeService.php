6d2211bb29b902192bdd3f180a2d862d1dc286ab
Adrian Borthomiert <fritoadn@gmail.com>
2022-07-22 12:55:25 -0400

    avances

--- app/Services/StripeService.php
+++ app/Services/StripeService.php
@@ -55,7 +55,7 @@ class StripeService
        
         
 
-        return $intent;
+        return redirect()->route('stripe-approval');
     }
 
     public function handleApproval()
@@ -111,63 +111,6 @@ class StripeService
             ->withErrors('We were unable to confirm your payment. Try again, please');
     }
 
-    public function handleSubscription(Request $request)
-    {
-        $customer = $this->createCustomer(
-            $request->user()->name,
-            $request->user()->email,
-            $request->payment_method
-        );
-
-        $subscription = $this->createSubscription(
-            $customer->id,
-            $request->payment_method,
-            $this->plans[$request->plan]
-        );
-
-        if ($subscription->status == 'active') {
-            session()->put('subscriptionId', $subscription->id);
-
-            return redirect()->route(
-                'subscribe.approval',
-                [
-                    'plan' => $request->plan,
-                    'subscription_id' => $subscription->id,
-                ],
-            );
-        }
-
-        $paymentIntent = $subscription->latest_invoice->payment_intent;
-
-        if ($paymentIntent->status === 'requires_action') {
-            $clientSecret = $paymentIntent->client_secret;
-
-            session()->put('subscriptionId', $subscription->id);
-
-            return view('stripe.3d-secure-subscription')->with([
-                'clientSecret' => $clientSecret,
-                'plan' => $request->plan,
-                'paymentMethod' => $request->payment_method,
-                'subscriptionId' => $subscription->id,
-            ]);
-        }
-
-        return redirect()->route('subscribe.show')
-            ->withErrors('We were unable to activate your subscription. Try again, please.');
-    }
-
-    public function validateSubscription(Request $request)
-    {
-        if (session()->has('subscriptionId')) {
-            $subscriptionId = session()->get('subscriptionId');
-
-            session()->forget('subscriptionId');
-
-            return $request->subscription_id == $subscriptionId;
-        }
-
-        return false;
-    }
 
     public function createIntent($value, $currency, $paymentMethod)
     {
@@ -206,22 +149,6 @@ class StripeService
         );
     }
 
-    public function createSubscription($customerId, $paymentMethod, $priceId)
-    {
-        return $this->makeRequest(
-            'POST',
-            '/v1/subscriptions',
-            [],
-            [
-                'customer' => $customerId,
-                'items' => [
-                    ['price' => $priceId],
-                ],
-                'default_payment_method' => $paymentMethod,
-                'expand' => ['latest_invoice.payment_intent']
-            ],
-        );
-    }
 
     public function resolveFactor($currency)
     {
