98abd44cc986e5480a55be2366c41337ec024f8d
arevaloarcadio.96 <arevaloarcadio.96@gmail.com>
2022-08-03 17:39:43 -0400

    actualizando scripts

--- app/Console/Commands/ImportWebmobile24AdsCommand.php
+++ app/Console/Commands/ImportWebmobile24AdsCommand.php
@@ -667,7 +667,7 @@ class ImportWebmobile24AdsCommand extends Command
                             'source' => 'WEB_MOBILE_24',
                             'images_processing_status' => 'SUCCESSFUL'
                         ];
-                        $this->info( var_dump($year_month));
+                        
                         $data_auto_ad = [
                             'price' => $csv_ad[15],//OK.
                             'price_contains_vat' => 0,
@@ -688,8 +688,8 @@ class ImportWebmobile24AdsCommand extends Command
                             'ad_fuel_type_id' => $this->findFuelTypeId($csv_ad[12])->id, //OK
                             'ad_body_type_id' => $this->findBodyTypeId(utf8_encode($csv_ad[8]))->id, //OK
                             'ad_transmission_type_id' => $this->findTransmissionTypeId($csv_ad[7])->id, 
-                            'first_registration_year' =>   !is_array($year_month) ? now('Y') : $year_month[1],
-                            'first_registration_month' => !is_array($year_month) ? now('m') :  $year_month[0], 
+                            'first_registration_year' =>   count($year_month) == 0 ? now('Y') : $year_month[1],
+                            'first_registration_month' => count($year_month) == 0 ? now('m') :  $year_month[0], 
                             'engine_displacement' => $csv_ad[18]  == '' ? 0 : $csv_ad[18], //OK
                             'power_hp' => $csv_ad[19], //OK
                             'make_id' => $this->findMake($csv_ad[3])->id, //OK
