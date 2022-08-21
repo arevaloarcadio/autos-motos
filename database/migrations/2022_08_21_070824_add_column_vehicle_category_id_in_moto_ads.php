a6af888660f911fa1271fe4262211c71d580464b
arevaloarcadio.96 <arevaloarcadio.96@gmail.com>
2022-08-21 01:19:13 -0400

    data

--- database/migrations/2022_08_21_070824_add_column_vehicle_category_id_in_moto_ads.php
+++ database/migrations/2022_08_21_070824_add_column_vehicle_category_id_in_moto_ads.php
@@ -14,7 +14,7 @@ class AddColumnVehicleCategoryIdInMotoAds extends Migration
     public function up()
     {
         Schema::table('moto_ads', function (Blueprint $table) {
-             $table->uuid('vehicle_category_id ')->nullable();
+             $table->uuid('vehicle_category_id')->nullable();
         });
     }
 
