d43d0af6262887beabd135b89d2305dd99249515
arevaloarcadio.96 <arevaloarcadio.96@gmail.com>
2022-10-12 01:51:43 -0400

    inve

--- app/Console/Commands/ImportInventarioAdsCommand.php
+++ app/Console/Commands/ImportInventarioAdsCommand.php
@@ -383,11 +383,6 @@ class ImportInventarioAdsCommand extends Command
                 $dealer->save();
             }
             
-            $dealer->logo_path = $seller->cliente_logo;
-            $dealer->save();
-            
-            $this->info(var_dump($seller) );      
-            
             return $dealer;
         }
 
@@ -398,7 +393,7 @@ class ImportInventarioAdsCommand extends Command
             'zip_code'      => (string) $seller->cliente_codigo_postal,
             'city'          => (string) $seller->cliente_provincia,
             'country'       => self::COUNTRY_NAME,
-            'logo_path'     => $seller->cliente_logo,
+            'logo_path'     => null,
             'email_address' => (string) $seller->cliente_email,
             'phone_number'  => $this->formatPhoneNumber((string) $seller->cliente_telefono_primero),
             'source'        => AdSourceEnum::INVENTARIO_IMPORT,
