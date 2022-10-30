bfd6e402779b36239636a0618db5db5f75f2f1be
arevaloarcadio.96 <arevaloarcadio.96@gmail.com>
2022-10-30 14:09:35 -0400

    a

--- app/Console/Commands/Users.php
+++ app/Console/Commands/Users.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Hash;
-use App\Models\{Dealer,User};
+use App\Models\{Dealer,RentalAd,Ad,DealerShowRoom,User};
 
 class Users extends Command
 {
@@ -39,41 +39,45 @@ class Users extends Command
      */
     public function handle()
     {
-        $dealers = Dealer::select('email_address')
-            ->groupBy('email_address')
-            ->havingRaw('count(email_address)>1')
-            ->get();
+        $tables = ['mechanic_ads','rental_ads'];
 
-      
-        foreach ($dealers as $dealer) {
-            
-            $d = Dealer::where('email_address',$dealer->email_address)->first();
+        foreach ($tables as $table) {
 
-            $val = User::where('email',$dealer->email_address)->first();
-            
-            if(!is_null($val) || $dealer->email_address == 'pruebasoftware.22@gmail.com'){
-                continue;
+            $dealers = Dealer::whereRaw("email_address IN(select email_address from ".$table.")")->get();
+
+            foreach ($dealers as $dealer) {
+                
+                $user = User::where('email',$dealer->email_address)->first();
+                 
+                $this->info($dealer->company_name);  
+                
+                if (is_null($user)) {
+                    $user = new User;
+                    $user->first_name =  $dealer->company_name;
+                    $user->last_name =  '.';
+                    $user->mobile_number =  $dealer->phone_number;
+                    $user->landline_number =  '+000000000';
+                    $user->whatsapp_number =  '+000000000';
+                    $user->email  =  $dealer->email_address;
+                    $user->email_verified_at =  null;
+                    $user->password =  Hash::make($dealer->company_name.'123456789**');
+                    $user->dealer_id  =  $dealer->id;
+                    $user->remember_token =  null;
+                    $user->type =  'Profesional';
+                    $user->code_postal =  $dealer->zip_code;
+                    $user->status =  'Aprobado';
+                    $user->image =  'users/user-default-ocassional.png' ;
+                    $user->save();
+                }
+                
+                $dealer_show_room = DealerShowRoom::where('dealer_id',$dealer->id)->first();
+
+                RentalAd::where('email_address',$dealer->email_address)
+                    ->update(['dealer_show_room_id' => $dealer_show_room->id ?? null,'dealer_id' => $dealer->id]);
+
+                Ad::whereRaw("id in(select ad_id from rental_ads where email_address = '".$dealer->email_address."')")
+                    ->update(['user_id' => $user->id]);
             }
-
-            $user = new User;
-            $user->first_name =  $d->company_name;
-            $user->last_name =  '.';
-            $user->mobile_number =  $d->phone_number;
-            $user->landline_number =  '+000000000';
-            $user->whatsapp_number =  '+000000000';
-            $user->email  =  $d->email_address;
-            $user->email_verified_at =  null;
-            $user->password =  Hash::make($d->company_name.'123456789**');
-            $user->dealer_id  =  $d->id;
-            $user->remember_token =  null;
-            $user->type =  'Profesional';
-            $user->code_postal =  $d->zip_code;
-            $user->status =  'Aprobado';
-            $user->image =  'users/user-default-ocassional.png' ;
-            $user->save();
-
-            $this->info($d->email_address);  
-             
         }
     }
 }
