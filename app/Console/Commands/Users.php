60f49caf6b6758e368ae1928e37589e39a32d9ba
arevaloarcadio.96 <arevaloarcadio.96@gmail.com>
2022-11-15 17:27:48 -0400

    asignacion de codigo de empresa

--- app/Console/Commands/Users.php
+++ app/Console/Commands/Users.php
@@ -39,45 +39,17 @@ class Users extends Command
      */
     public function handle()
     {
-        $tables = ['mechanic_ads','rental_ads'];
+        $dealers = Dealer::whereRaw('code is null')->get();
 
-        foreach ($tables as $table) {
+        foreach ($dealers as $dealer) {
 
-            $dealers = Dealer::whereRaw("email_address IN(select email_address from ".$table.")")->get();
-
-            foreach ($dealers as $dealer) {
-                
-                $user = User::where('email',$dealer->email_address)->first();
-                 
-                $this->info($dealer->company_name);  
-                
-                if (is_null($user)) {
-                    $user = new User;
-                    $user->first_name =  $dealer->company_name;
-                    $user->last_name =  '.';
-                    $user->mobile_number =  $dealer->phone_number;
-                    $user->landline_number =  '+000000000';
-                    $user->whatsapp_number =  '+000000000';
-                    $user->email  =  $dealer->email_address;
-                    $user->email_verified_at =  null;
-                    $user->password =  Hash::make($dealer->company_name.'123456789**');
-                    $user->dealer_id  =  $dealer->id;
-                    $user->remember_token =  null;
-                    $user->type =  'Profesional';
-                    $user->code_postal =  $dealer->zip_code;
-                    $user->status =  'Aprobado';
-                    $user->image =  'users/user-default-ocassional.png' ;
-                    $user->save();
-                }
-                
-                $dealer_show_room = DealerShowRoom::where('dealer_id',$dealer->id)->first();
-
-                RentalAd::where('email_address',$dealer->email_address)
-                    ->update(['dealer_show_room_id' => $dealer_show_room->id ?? null,'dealer_id' => $dealer->id]);
-
-                Ad::whereRaw("id in(select ad_id from rental_ads where email_address = '".$dealer->email_address."')")
-                    ->update(['user_id' => $user->id]);
-            }
+            $count = Dealer::whereRaw('code is not null')->count();
+            
+            $this->info($dealer->company_name."->".$count);
+            
+            $dealer->code = str_pad($code, 5, "0",STR_PAD_LEFT);
+            
+            $dealer->save();
         }
     }
 }
